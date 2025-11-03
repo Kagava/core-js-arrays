f2adbf5cebe748cc8810a3709c2912d1d482a3ab
Kagava <esspakov@mail.ru>
2025-11-03 14:21:55 +0700

    feat: complete getIntervalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,11 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  if (end < start) return [];
+  const length = end - start + 1;
+
+  return Array.from({ length }, (_, i) => start + i);
 }
 
 /**
